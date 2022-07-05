59badd365794c2fe0945bb2c2ae0d264a74384a5
unnamedguy <unnamedjguy@icloud.com>
2022-07-05 18:27:32 +0300

    Remove unneeded methods in CarRepository class

--- src/main/java/com/unnamedguy/resttest/repository/CarRepository.java
+++ src/main/java/com/unnamedguy/resttest/repository/CarRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
-    List<Car> findByName(String name);
-
-    List<Car> findByEmail(String email);
 }
